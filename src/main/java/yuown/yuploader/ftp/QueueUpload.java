1ad7632e94e7d09b37c17f3e6e89f028723f23ce
yenkaykiran <yenkaykiran@gmail.com>
2015-07-14 08:06:43 +0530

    14-JULY-2015 - Fix Pause for Network Disconnection

--- src/main/java/yuown/yuploader/ftp/QueueUpload.java
+++ src/main/java/yuown/yuploader/ftp/QueueUpload.java
@@ -1,5 +1,13 @@
 package yuown.yuploader.ftp;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.channels.Channels;
+
+import javax.swing.SwingWorker;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
@@ -16,123 +24,135 @@ import yuown.yuploader.model.YuploaderTableModel;
 import yuown.yuploader.ui.Client;
 import yuown.yuploader.util.Helper;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.RandomAccessFile;
-import java.nio.channels.Channels;
-
-import javax.swing.SwingWorker;
-
 @Component
 @Scope("prototype")
 public class QueueUpload extends SwingWorker<Integer, Integer> {
-    @Autowired
-    private YuploaderTableModel yuploaderTableModel;
-    @Autowired
-    private FTPClient ftpClient;
-    @Autowired
-    private Client client;
-    @Autowired
-    private StreamListener streamListener;
-    @Autowired
-    private Helper helper;
+	@Autowired
+	private YuploaderTableModel yuploaderTableModel;
+	@Autowired
+	private FTPClient ftpClient;
+	@Autowired
+	private Client client;
+	@Autowired
+	private StreamListener streamListener;
+	@Autowired
+	private Helper helper;
 
-    @Value("${ftp.conn.host}")
-    private String ftpHost;
+	@Value("${ftp.conn.host}")
+	private String ftpHost;
 
-    @Value("${ftp.conn.user}")
-    private String ftpUsername;
+	@Value("${ftp.conn.user}")
+	private String ftpUsername;
 
-    @Value("${ftp.conn.pass}")
-    private String ftpPassword;
+	@Value("${ftp.conn.pass}")
+	private String ftpPassword;
 
-    @Value("${ftp.conn.port}")
-    private int ftpPort;
+	@Value("${ftp.conn.port}")
+	private int ftpPort;
 
-    @Value("${ftp.bufferSize}")
-    private int ftpBufferSize;
+	@Value("${ftp.bufferSize}")
+	private int ftpBufferSize;
 
-    @Value("${ftp.conn.basepath}")
-    private String ftpBasePath;
+	@Value("${ftp.conn.basepath}")
+	private String ftpBasePath;
 
-    @Autowired
-    private User userObject;
+	@Autowired
+	private User userObject;
 
-    public void submitToQueue() {
-        client.toggleLogin(false);
-        System.out.println("Uploader Worker Started: " + System.currentTimeMillis());
+	public void submitToQueue() {
+		client.toggleLogin(false);
+		System.out.println("Uploader Worker Started: " + System.currentTimeMillis());
 
-        int rowCount = this.yuploaderTableModel.getRowCount();
-        for (int row = 0; (row < rowCount); row++) {
-            FileObject file = (FileObject) this.yuploaderTableModel.getValueAt(row, 0);
-            File f = new File(file.getFullPath());
-            if ((f.exists()) && (isEligibleToStartUpload(file))) {
-                InputStream reader = getReader(f, file);
-                try {
-                    markUploadInProgress(file, row);
-                    client.togglePause(true);
-                    streamListener.setReader(reader);
-                    if (file.getOffset() > 0) {
-                        ftpClient.appendFile(file.getFileName(), reader);
-                    } else {
-                        ftpClient.storeFile(file.getFileName(), reader);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    break;
-                } finally {
-                    Util.closeQuietly(reader);
-                }
-            }
-        }
-        client.togglePause(false);
-        client.toggleLogin(true);
-    }
+		int rowCount = this.yuploaderTableModel.getRowCount();
+		for (int row = 0; (row < rowCount); row++) {
+			FileObject file = (FileObject) this.yuploaderTableModel.getValueAt(row, 0);
+			File f = new File(file.getFullPath());
+			if ((f.exists()) && (isEligibleToStartUpload(file))) {
+				InputStream reader = getReader(f, file);
+				try {
+					markUploadInProgress(file, row);
+					client.togglePause(true);
+					streamListener.setReader(reader);
+					if (file.getOffset() > 0) {
+						ftpClient.appendFile(file.getFileName(), reader);
+					} else {
+						ftpClient.storeFile(file.getFileName(), reader);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+					break;
+				} finally {
+					Util.closeQuietly(reader);
+				}
+			}
+		}
+		client.togglePause(false);
+		client.toggleLogin(true);
+	}
 
-    private long getOffsetFromServer(FileObject file) throws IOException {
-        long l = -1;
-        FTPFile[] ftpFile = ftpClient.listFiles();
-        for (int i = 0; i < ftpFile.length; i++) {
-            if (StringUtils.equals(ftpFile[i].getName(), file.getFileName())) {
-                l = ftpFile[i].getSize();
-                break;
-            }
-        }
-        if (l < 0) {
-            l = 0;
-        }
-        return l;
-    }
+	private long getOffsetFromServer(FileObject file) throws IOException {
+		long l = -1;
+		FTPFile[] ftpFile = ftpClient.listFiles();
+		for (int i = 0; i < ftpFile.length; i++) {
+			if (StringUtils.equals(ftpFile[i].getName(), file.getFileName())) {
+				l = ftpFile[i].getSize();
+				break;
+			}
+		}
+		if (l < 0) {
+			l = 0;
+		}
+		return l;
+	}
 
-    @SuppressWarnings("resource")
-    protected InputStream getReader(File f, FileObject file) {
-        InputStream reader = null;
-        try {
-            if (file.getOffset() > 0) {
-                file.setOffset(getOffsetFromServer(file));
-            }
-            RandomAccessFile raf = new RandomAccessFile(f, "r");
-            reader = Channels.newInputStream(raf.getChannel().position(file.getOffset()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return reader;
-    }
+	@SuppressWarnings("resource")
+	protected InputStream getReader(File f, FileObject file) {
+		InputStream reader = null;
+		try {
+			if (file.getOffset() > 0) {
+				file.setOffset(getOffsetFromServer(file));
+			}
+			RandomAccessFile raf = new RandomAccessFile(f, "r");
+			reader = Channels.newInputStream(raf.getChannel().position(file.getOffset()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return reader;
+	}
 
-    private void markUploadInProgress(FileObject file, int row) {
-        this.yuploaderTableModel.setValueAt(Status.IN_PROGRESS, row, 3);
-        file.setStatus(Status.IN_PROGRESS);
-        ftpClient.setCopyStreamListener(streamListener);
-        this.streamListener.setRow(row);
-    }
+	private void markUploadInProgress(FileObject file, int row) {
+		this.yuploaderTableModel.setValueAt(Status.IN_PROGRESS, row, 3);
+		file.setStatus(Status.IN_PROGRESS);
+		ftpClient.setCopyStreamListener(streamListener);
+		streamListener.setRow(row);
 
-    protected boolean isEligibleToStartUpload(FileObject file) {
-        return !StringUtils.equals(Status.COMPLETED.toString(), file.getStatus().toString());
-    }
+//		new SwingWorker<Integer, Integer>() {
+//			@Override
+//			protected Integer doInBackground() throws Exception {
+//				long t = System.currentTimeMillis();
+//				while (streamListener.isStarted()) {
+//					long d = System.currentTimeMillis() - t;
+//					if (d > 10000L) {
+//						t = System.currentTimeMillis();
+//						boolean reachable = streamListener.isNetworkConnected();
+//						if (!reachable) {
+//							streamListener.actionPerformed(null);
+//							streamListener.setStarted(false);
+//						}
+//					}
+//				}
+//				return 0;
+//			}
+//		}.execute();
 
-    protected Integer doInBackground() throws Exception {
-        submitToQueue();
-        return Integer.valueOf(0);
-    }
+	}
+
+	protected boolean isEligibleToStartUpload(FileObject file) {
+		return !StringUtils.equals(Status.COMPLETED.toString(), file.getStatus().toString());
+	}
+
+	protected Integer doInBackground() throws Exception {
+		submitToQueue();
+		return Integer.valueOf(0);
+	}
 }
--- src/main/java/yuown/yuploader/ftp/StreamListener.java
+++ src/main/java/yuown/yuploader/ftp/StreamListener.java
@@ -1,10 +1,20 @@
 package yuown.yuploader.ftp;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.io.CopyStreamEvent;
 import org.apache.commons.net.io.CopyStreamListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -16,121 +26,146 @@ import yuown.yuploader.ui.Client;
 import yuown.yuploader.util.Helper;
 import yuown.yuploader.util.YuownUtils;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.swing.JOptionPane;
-
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class StreamListener implements CopyStreamListener, ActionListener {
 
-    @Autowired
-    private YuploaderTableModel yuploaderTableModel;
+	@Autowired
+	private YuploaderTableModel yuploaderTableModel;
 
-    @Autowired
-    private Client client;
+	@Autowired
+	private Client client;
 
-    @Autowired
-    private FTPClient ftpClient;
+	@Autowired
+	private FTPClient ftpClient;
 
-    private long bytes = 0;
-    private double percentCompleted;
-    private long time = System.currentTimeMillis();
-    private long startTime = System.currentTimeMillis();
+	@Value("${ftp.conn.host}")
+	private String ftpHost;
 
-    private int row = -1;
+	@Value("${ftp.conn.port}")
+	private int ftpPort;
 
-    private FileObject fileObject;
+	public static final int TIMEOUT = 2000;
 
-    private long totalBytesTransferred;
+	private long bytes = 0;
+	private double percentCompleted;
+	private long time = System.currentTimeMillis();
+	private long startTime = System.currentTimeMillis();
 
-    @Autowired
-    private Helper helper;
+	private int row = -1;
 
-    private InputStream reader;
+	private FileObject fileObject;
 
-    public StreamListener() {
-    }
+	private long totalBytesTransferred;
 
-    private void updateProgress(int row) {
-        double currentKBRate = YuownUtils.longTo2Decimals(this.bytes, 1024);
-        if (currentKBRate < 1024) {
-            yuploaderTableModel.setValueAt(currentKBRate + " KB/s", row, 4);
-        } else {
-            double currentMBRate = YuownUtils.longTo2Decimals(currentKBRate, 1024);
-            yuploaderTableModel.setValueAt(currentMBRate + " MB/s", row, 4);
-        }
-        yuploaderTableModel.setValueAt(YuownUtils.longTo2Decimals(this.time - this.startTime, 1000), row, 5);
-        yuploaderTableModel.setValueAt(this.percentCompleted + " %", row, 2);
-        client.setLastAccess(System.currentTimeMillis());
-    }
+	@Autowired
+	private Helper helper;
 
-    public void setRow(int row2) {
-        this.row = row2;
-        fileObject = (FileObject) yuploaderTableModel.getValueAt(row, 0);
-        this.startTime = System.currentTimeMillis();
-        totalBytesTransferred = fileObject.getOffset();
-    }
+	private InputStream reader;
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        try {
-            client.toggleLogin(false);
-            reader.close();
-            ftpClient.abort();
-            ftpClient.completePendingCommand();
-        } catch (IOException ex) {
-            client.toggleLogin(true);
-            client.togglePause(false);
-            ex.printStackTrace();
-        }
-        if (StringUtils.equals("Cancel Upload", e.getActionCommand())) {
-            int choice = helper.confirm(client, "Are you sure cancel Current Upload ?");
-            if (choice == JOptionPane.YES_OPTION) {
-                fileObject.setOffset(0);
-                yuploaderTableModel.setValueAt(Status.ADDED, row, 3);
-                yuploaderTableModel.setValueAt("0 %", row, 2);
-                totalBytesTransferred = 0;
-            }
-        }
-    }
+	public StreamListener() {
+	}
 
-    @Override
-    public void bytesTransferred(CopyStreamEvent event) {
-        bytesTransferred(event.getTotalBytesTransferred(), event.getBytesTransferred(), event.getStreamSize());
-    }
+	private void updateProgress(int row) {
+		double currentKBRate = YuownUtils.longTo2Decimals(this.bytes, 1024);
+		if (currentKBRate < 1024) {
+			yuploaderTableModel.setValueAt(currentKBRate + " KB/s", row, 4);
+		} else {
+			double currentMBRate = YuownUtils.longTo2Decimals(currentKBRate, 1024);
+			yuploaderTableModel.setValueAt(currentMBRate + " MB/s", row, 4);
+		}
+		yuploaderTableModel.setValueAt(YuownUtils.longTo2Decimals(this.time - this.startTime, 1000), row, 5);
+		yuploaderTableModel.setValueAt(this.percentCompleted + " %", row, 2);
+		client.setLastAccess(System.currentTimeMillis());
+	}
 
-    @Override
-    public void bytesTransferred(long totalBytesTransferred, int bytesTransferred, long streamSize) {
-        long s = fileObject.getSize();
-        totalBytesTransferred += this.totalBytesTransferred;
-        this.percentCompleted = YuownUtils.longTo2Decimals(totalBytesTransferred * 100, s);
-        long d = System.currentTimeMillis() - this.time;
-        if (d > 1000L) {
-            this.time = System.currentTimeMillis();
-            updateProgress(row);
-            this.bytes = 0L;
-        } else {
-            this.bytes += bytesTransferred;
-        }
-        yuploaderTableModel.setValueAt(this.percentCompleted + " %", row, 2);
-        if (s == totalBytesTransferred) {
-            client.togglePause(false);
-            updateProgress(row);
-            fileObject.setStatus(Status.COMPLETED);
-            yuploaderTableModel.setValueAt(Status.COMPLETED, row, 3);
-        }
-        fileObject.setOffset(totalBytesTransferred);
-    }
+	public void setRow(int row2) {
+		this.row = row2;
+		fileObject = (FileObject) yuploaderTableModel.getValueAt(row, 0);
+		this.startTime = System.currentTimeMillis();
+		totalBytesTransferred = fileObject.getOffset();
+	}
 
-    public void setTotalBytesTransferred(long totalBytesTransferred) {
-        this.totalBytesTransferred = totalBytesTransferred;
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		try {
+			client.toggleLogin(false);
+			reader.close();
+			boolean reachable = isNetworkConnected();
+			if (reachable) {
+				ftpClient.abort();
+				ftpClient.completePendingCommand();
+			}
+			client.setConnected(reachable);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		client.toggleLogin(true);
+		client.togglePause(false);
+		if (StringUtils.equals("Cancel Upload", e.getActionCommand())) {
+			int choice = helper.confirm(client, "Are you sure cancel Current Upload ?");
+			if (choice == JOptionPane.YES_OPTION) {
+				fileObject.setOffset(0);
+				yuploaderTableModel.setValueAt(Status.ADDED, row, 3);
+				yuploaderTableModel.setValueAt("0 %", row, 2);
+				totalBytesTransferred = 0;
+			}
+		}
+	}
 
-    public void setReader(InputStream reader) {
-        this.reader = reader;
-    }
+	@Override
+	public void bytesTransferred(CopyStreamEvent event) {
+		bytesTransferred(event.getTotalBytesTransferred(), event.getBytesTransferred(), event.getStreamSize());
+	}
+
+	@Override
+	public void bytesTransferred(long totalBytesTransferred, int bytesTransferred, long streamSize) {
+		long s = fileObject.getSize();
+		totalBytesTransferred += this.totalBytesTransferred;
+		this.percentCompleted = YuownUtils.longTo2Decimals(totalBytesTransferred * 100, s);
+		long d = System.currentTimeMillis() - this.time;
+		if (d > 1000L) {
+			this.time = System.currentTimeMillis();
+			updateProgress(row);
+			this.bytes = 0L;
+		} else {
+			this.bytes += bytesTransferred;
+		}
+		yuploaderTableModel.setValueAt(this.percentCompleted + " %", row, 2);
+		if (s == totalBytesTransferred) {
+			client.togglePause(false);
+			updateProgress(row);
+			fileObject.setStatus(Status.COMPLETED);
+			yuploaderTableModel.setValueAt(Status.COMPLETED, row, 3);
+		}
+		fileObject.setOffset(totalBytesTransferred);
+	}
+
+	public void setTotalBytesTransferred(long totalBytesTransferred) {
+		this.totalBytesTransferred = totalBytesTransferred;
+	}
+
+	public void setReader(InputStream reader) {
+		this.reader = reader;
+	}
+
+	public boolean isNetworkConnected() {
+		Socket socket = null;
+		boolean reachable = false;
+		try {
+			socket = new Socket(ftpHost, ftpPort);
+			reachable = true;
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (socket != null)
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+		}
+		return reachable;
+	}
 }
